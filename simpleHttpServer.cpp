87f4c4b43a95f53651dc6caae5a45b5dae3a6da8
ishandahal <ishan.dahal88@gmail.com>
2023-09-02 17:43:14 -0400

    Add methods to send response

--- simpleHttpServer.cpp
+++ simpleHttpServer.cpp
@@ -80,4 +80,42 @@ int TcpServer::start_server() {
   }
   return 0;
 }
+
+void TcpServer::accept_connection(int &new_socket) {
+  new_socket =
+      accept(m_socket, (sockaddr *)&m_socket_address, &m_socket_address_len);
+  if (new_socket < 0) {
+    std::ostringstream ss;
+    ss << "Server failed to accept connection from ADDRESS: "
+       << inet_ntoa(m_socket_address.sin_addr)
+       << "; PORT: " << ntohs(m_socket_address.sin_port);
+    exit_with_error(ss.str());
+  }
+}
+
+std::string TcpServer::build_response() {
+  std::string html_file =
+      "<!DOCTYPE html><html lang=\"en\"><body><h1> HOME </h1><p> Hello from "
+      "your Server :) </p></body></html>";
+  std::ostringstream ss;
+  ss << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-length: "
+     << html_file.size() << "\n\n"
+     << html_file;
+
+  return ss.str();
+}
+
+void TcpServer::send_response() {
+  long bytes_sent;
+
+  bytes_sent =
+      write(m_new_socket, m_server_message.c_str(), m_server_message.size());
+
+  if (bytes_sent == m_server_message.size()) {
+    log("------ Server Response sent to client ------\n\n");
+  } else {
+    log("error sending Response to client");
+  }
+}
+
 } // namespace server
